4d21393bd0cc13d597ac3aa371197f6df3cfb2b6
lindb3 <805716627@qq.com>
2017-06-22 22:21:57 +0800

    Create ShareActivity.java

---
+++ client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/more/ShareActivity.java
@@ -0,0 +1,32 @@
+package com.yinghangjiaclient.more;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.orhanobut.logger.Logger;
+import com.yinghangjiaclient.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Logger.init("ying");
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_share);
+
+            Button backBtn = (Button) findViewById(R.id.button3);
+            backBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.e(e.getMessage());
+        }
+    }
+}
