b4d0e5f62cf73f6b361ad1c1104e92c428a1db42
zhuangqh <zhuangqhc@163.com>
2017-06-23 21:26:16 +0800

    fix some bug

--- server/src/controllers/news.js
+++ server/src/controllers/news.js
@@ -17,6 +17,41 @@ async function getNews(ctx) {
   };
 }
 
+async function getFeedBack(ctx) {
+  const query = ctx.request.query;
+
+  let news = await feedBackService.getFeedBack(query.page, query.dir)
+    .catch((err) => {
+      ctx.body = {
+        err: err,
+      };
+    });
+
+  if (news && news.length > 0) {
+    ctx.body = {
+      data: news,
+    };
+  } else {
+    ctx.response.status = 404;
+  }
+}
+
+async function addFeedBack(ctx) {
+  const body = ctx.request.body;
+
+  if (hasEvery(body, ['username', 'text'])) {
+    await feedBackService.addFeedBack(body)
+      .catch((err) => {
+        error.error(err);
+        ctx.response.status = 403;
+      });
+
+    ctx.response.status = 200;
+  } else {
+    ctx.response.status = 400;
+  }
+}
+
 export default function newsCtrl(router) {
   router.get('/api/news', getNews);
 };
