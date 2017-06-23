b3d099d36d36f520cb9f8f3ca2071590d8cb6170
zhuangqh <zhuangqhc@163.com>
2017-06-23 21:26:53 +0800

    duplicate

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -38,6 +38,29 @@ async function signin(ctx) {
   }
 }
 
+async function comment(ctx) {
+  const id = ctx.params.id;
+  const body = ctx.request.body;
+
+  if (isDefined(id, body.username, body.text)) {
+    await prodService.addComment(id, body);
+
+    ctx.response.status = 200;
+  } else {
+    ctx.response.status = 400;
+  }
+}
+
+async function getAllProd(ctx) {
+  const query = ctx.request.query;
+
+  const prods = await prodService.findAllProd(query);
+
+  ctx.body = {
+    data: prods,
+  };
+}
+
 async function star(ctx) {
   const userId = ctx.params.id;
   const type = ctx.request.body.type;
