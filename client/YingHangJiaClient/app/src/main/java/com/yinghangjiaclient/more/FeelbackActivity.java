90604b7ed8d905fcad2e974af74407b1b968dc92
lindb3 <805716627@qq.com>
2017-06-23 16:09:21 +0800

    Update FeelbackActivity.java

--- client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/more/FeelbackActivity.java
+++ client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/more/FeelbackActivity.java
@@ -49,6 +49,7 @@ public class FeelbackActivity extends AppCompatActivity {
 
             
             Button backBtn = (Button) findViewById(R.id.button3);
+            
             backBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
