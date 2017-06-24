acf8d989986477f1de3acb2027c2bd1731e1549c
LINMouMouZiBo <673418747@qq.com>
2017-06-24 14:59:12 +0800

    create mangaer-server project

--- client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/MainActivity.java
+++ client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/MainActivity.java
@@ -1,7 +1,9 @@
-package com.yinghangjiaclient.yinghangjiaclient;
+package com.yinghangjiaclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.yinghangjiaclient.yinghangjiaclient.R;
 
 public class MainActivity extends AppCompatActivity {
 
