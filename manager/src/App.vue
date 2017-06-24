037edb1459e227148f67a90b8a08218da5b44e00
zhuangqh <zhuangqhc@163.com>
2017-06-24 17:53:46 +0800

    main

---
+++ manager/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <feedback></feedback>
+</template>
+
+<script>
+import Feedback from './components/Feedback'
+
+export default {
+  components: {
+    Feedback
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+}
+
+</style>
