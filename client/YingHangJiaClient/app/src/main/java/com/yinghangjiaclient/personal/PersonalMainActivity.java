db51ec8b93331f92a0c9d8ce6111eea9c384cc3e
LINMouMouZiBo <673418747@qq.com>
2017-06-24 15:56:36 +0800

    add personal info and password edit

--- client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/personal/PersonalMainActivity.java
+++ client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/personal/PersonalMainActivity.java
@@ -1,5 +1,9 @@
 package com.yinghangjiaclient.personal;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,15 +13,16 @@ public class PersonalMainActivity extends AppCompatActivity {
     private SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_main);
-
-        sp = getSharedPreferences("userInfo", Activity.MODE_PRIVATE);
-        if (!sp.getBoolean("loginState", false)) {
-            Intent intent = new Intent();
-            intent.setClass(PersonalMainActivity.this, LoginActivity.class);
-            startActivity(intent);
-        }
+        Logger.init("ying");
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.mine_first);
+            sp = getSharedPreferences("userInfo", Activity.MODE_PRIVATE);
+            if (!sp.getBoolean("loginState", false)) {
+                Intent intent = new Intent();
+                intent.setClass(PersonalMainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
 
         TextView textView = (TextView) findViewById(R.id.textView8);
         textView.setText("用户名: " + sp.getString("USERNAME", ""));
@@ -66,5 +71,40 @@ public class PersonalMainActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+            // 跳转到个人信息
+            Button persenal_info_change_button = (Button) findViewById(R.id.persenal_info_change_button);
+            persenal_info_change_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent1 = new Intent();
+                    intent1.setClass(PersonalCenterActivity.this,
+                            PersonalInfoActivity.class);
+                    startActivity(intent1);
+                }
+            });
+
+            // 跳转到积分
+            Button score_btn = (Button) findViewById(R.id.score_btn);
+            score_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent1 = new Intent();
+                    intent1.setClass(PersonalMainActivity.this,
+                            ScoreActivity.class);
+                    startActivity(intent1);
+                }
+            });
+
+            Button backBtn = (Button) findViewById(R.id.button3);
+            backBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.e(e.getMessage());
+        }
     }
 }
