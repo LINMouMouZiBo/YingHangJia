a11779693627cd9ba9330541cda73769cbfe14ce
LINMouMouZiBo <673418747@qq.com>
2017-06-24 15:59:34 +0800

    add personal score and news advisor

---
+++ client/YingHangJiaClient/app/src/main/java/com/yinghangjiaclient/personal/ScoreActivity.java
@@ -0,0 +1,15 @@
+package com.yinghangjiaclient.personal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yinghangjiaclient.yinghangjiaclient.R;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
